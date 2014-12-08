d7c51d6c32578918665d19536ac1775788ecc40d
Gary Healy <gary.healy2@student.dit.ie>
2014-12-08 14:28:43 +0000

    08/12/2014

---
+++ Picture_Box/src/ie/dit/student/healy2/gary/Delete.java
@@ -0,0 +1,31 @@
+package ie.dit.student.healy2.gary;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+
+public class Delete extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	
+	//Create a blobstore service (for serving data)
+	private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+	
+	// file Serve.java
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws IOException
+	{
+		//Blob key is passed to the download handler
+		BlobKey blobKey = new BlobKey(req.getParameter("blob-key"));
+		
+		//Serve the blob key to the user
+		blobstoreService.delete(blobKey); 
+	}
+}
--- Picture_Box/src/ie/dit/student/healy2/gary/Picture_BoxServlet.java
+++ Picture_Box/src/ie/dit/student/healy2/gary/Picture_BoxServlet.java
@@ -1,9 +1,7 @@
 package ie.dit.student.healy2.gary;
 	
-import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesServiceFactory;
-import com.google.appengine.api.images.Transform;
 
 import java.io.IOException;
 import java.security.Principal;
@@ -16,8 +14,6 @@ import javax.servlet.http.*;
 import com.google.appengine.api.blobstore.BlobInfo;
 import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
-import com.google.appengine.api.blobstore.BlobstoreService;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
@@ -27,61 +23,57 @@ public class Picture_BoxServlet extends HttpServlet
 {
 	private String key;
 	private BlobKey bkey;
-	private int count = 0;
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws IOException 
 	{
 		resp.setContentType("text/plain");
 		 
-		//Create an implementation of the user service
 		UserService userService = UserServiceFactory.getUserService(); 
-		
-		//Get the name of the current aunthenticated user
 		Principal myPrincipal = req.getUserPrincipal();
-		
-		//Variable for email address initialised to null
-		String emailAddress = null; 
-		
-		//Variable to store the downloads page 
-		String downloadsPage = "/download.jsp";
-		
-		//Get the part of the URL used by the client to call the servlet on the server 
+		String emailAddress = null;  
+		String downloadsPage = "/download.jsp"; 
 		String thisURL = req.getRequestURI(); 
-		
-		//Get the login URL
 		String loginURL = userService.createLoginURL(thisURL);
-		
-		//Get the logout URL
 		String logoutURL = userService.createLogoutURL(thisURL);
-		
-		//Set the content type
 		resp.setContentType("text/html"); 
 		
-		//If there is no authenticated user
 		if(myPrincipal == null)
 		{
-			//Print appropriate messages
-			resp.getWriter().println("<p>You are not logged in time </p>");
-			
-			//Link to login
-			resp.getWriter().println("<p> you can <a href=\"" + loginURL + "\"> sign in here</a>.</p>");
+			resp.getWriter().println("<p>Welcome Guest</p>");
+			resp.getWriter().println("<p> You can <a href=\"" + loginURL + "\"> sign in here</a>.</p>");
 		}
-		
-		//If there is an authenticated user logged in 
+
 		if (myPrincipal != null)
 		{
-			//Get the email address of the current user
-			emailAddress = myPrincipal.getName(); 
+			emailAddress = myPrincipal.getName();
+
+			//Admin
+			if (emailAddress.equals("gazh1987gae@gmail.com") || emailAddress.equals("mark.deegan@dit.ie"))
+			{
+				resp.getWriter().println("<p>Welcome Administrator</p>");
+				resp.getWriter().println("<p> You are logged in as (email): " +emailAddress + "</p>"); 
+				resp.getWriter().println("<p> You can <a href=\"" + logoutURL + "\"> sign out here</a>.</p>");
+				resp.getWriter().println ("<p><a href=\"" + downloadsPage + "\"> Upload Image </a></p>"); 
+			}
 			
-			//print the email
-			resp.getWriter().println("<p> You are logged in as (email): " +emailAddress + "</p>"); 
+			//Member
+			else if (emailAddress.equals("c12726809@mydit.ie") || emailAddress.equals("C12726809@mydit.ie") || 
+					emailAddress.equals("gary.healy2@student.dit.ie"))
+			{
+				resp.getWriter().println("<p>Welcome Member</p>");
+				resp.getWriter().println("<p> You are logged in as (email): " +emailAddress + "</p>"); 
+				resp.getWriter().println("<p> You can <a href=\"" + logoutURL + "\"> sign out here</a>.</p>");
+				resp.getWriter().println ("<p><a href=\"" + downloadsPage + "\"> Upload Image </a></p>");
+			}
 			
-			//Link to logout
-			resp.getWriter().println("<p> You can <a href=\"" + logoutURL + "\"> sign out</a>.</p>");
-			
-			//Link to downloads
-			resp.getWriter().println ("<p><a href=\"" + downloadsPage + "\"> Upload Image </a></p>"); 
+			//Guest 
+			else
+			{
+				resp.getWriter().println("<p>Sorry you are not permitted to sign in with this email address</p>");
+				resp.getWriter().println("<p> You are logged in as (email): " +emailAddress + "</p>");
+				resp.getWriter().println("<p> You can try <a href=\"" + loginURL + "\"> sign in again here</a>.</p>");
+			}
 		}
 		
 		List<BlobInfo> blobList = new LinkedList<BlobInfo>();
@@ -92,47 +84,26 @@ public class Picture_BoxServlet extends HttpServlet
 			blobList.add(iterator.next());
 		}
 		
-		//resp.getWriter().println("<table> <tr> <td> Name </td> <td> Filetype </td> <td> Size </td></tr>");
 		resp.getWriter().println("<table>");
-		
-		//Use this code to delete all blobs
-		/*BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-		for(BlobInfo blobInfo : blobList)
-			blobstoreService.delete(blobInfo.getBlobKey());*/
 
 		for (int i = 0; i < blobList.size(); i ++)
 		{	
 			key = blobList.get(i).getBlobKey().toString();
 			key = key.substring(10, key.length() - 1);
 			
-			//NOTE: Find a better solution to this when finished all other major functionality. This is here so
-			//the app doesnt try to display the image of the deleted image with this blob key. If this wasnt
-			//here the app would crash when trying to find this blob.
-			if (!key.equals("HxY0g9syIv33S_11YIAyoQ"))
-			{
-				bkey = blobList.get(i).getBlobKey();
+			bkey = blobList.get(i).getBlobKey();
+
+			ImagesService imagesService = ImagesServiceFactory.getImagesService();
+			@SuppressWarnings("deprecation")
+			String imageUrl = imagesService.getServingUrl(bkey);
+	
+			resp.getWriter().println("<tr><td><img src=" + imageUrl + " height=\"300\" width=\"300\">" 
+					+ "<br>FileName: " + blobList.get(i).getFilename() 
+					+ "<br>Content Type: " + blobList.get(i).getContentType()
+					+ "<br>Size: " + blobList.get(i).getSize() 
+					+ "<br><input onClick=\"location.href='serve?blob-key="+key+"'\" type=\"button\" value=\"View Larger Image\" />"
+					+ "<br><input onClick=\"location.href='delete?blob-key="+key+"'\" type=\"button\" value=\"Delete Image\" /></td></tr>");
 			
-				ImagesService imagesService = ImagesServiceFactory.getImagesService();
-				String imageUrl = imagesService.getServingUrl(bkey);
-			
-				if (count % 2 == 0)
-				{
-					resp.getWriter().println("<tr>");
-				}
-				count++;	
-				
-				resp.getWriter().println("<td><img src=" + imageUrl + " height=\"300\" width=\"300\">" 
-				+ "<br>FileName: " + blobList.get(i).getFilename() 
-				+ "<br>Content Type: " + blobList.get(i).getContentType()
-				+ "<br>Size: " + blobList.get(i).getSize() 
-				+ "<br>Blob Key: " + key
-				+ "<br><input onClick=\"location.href='serve?blob-key="+key+"'\" type=\"button\" value=\"Download\" /><br><br></td>");
-				
-				if (count % 2 == 0)
-				{
-					resp.getWriter().println("</tr>");
-				}
-			}
 		}
 		
 		resp.getWriter().println("</table>");
